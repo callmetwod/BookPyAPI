58246791e5e87d42c9853a2fa8ad71dd7839f904
2D <d4vidmenezes@gmail.com>
2024-07-09 17:12:09 -0300

    feat: 1.0 api version

---
+++ livros.py
@@ -0,0 +1,17 @@
+livros = [
+    {
+    'id': 1,
+    'título': 'teste',
+    'autor': 'teste autor',
+    },
+    {
+    'id': 2,
+    'título': 'teste2',
+    'autor': 'teste autor2',
+    },
+    {
+    'id': 3,
+    'título': 'teste3',
+    'autor': 'teste autor3',
+    },
+] 
